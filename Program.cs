5f50221b756251c93f3b82e42416e34b6dc71ddf
mihiraj27 <mihiraj@Mihirajs-MBP.localdomain>
2024-09-06 15:47:38 +1000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿// Console.WriteLine("Enter Your Name Here ");
+
+// string firtsName = Console.ReadLine();
+
+
+// Console.WriteLine("Your Name is "+firtsName);
+
+
+
+
+// int x = 100 + 50;
+
+// Console.WriteLine(x);
+
+
+// string firstName = "John";
+// string lastName = "Doe";
+// string name = $"My full name is: {firstName} {lastName}";
+// Console.WriteLine(name);
+
+
+
+// string txt = "We are the so-called \'Vikings\' from the north.";
+
+// Console.WriteLine(txt);
+
+
+// int myAge = 25;
+// int votingAge = 37;
+
+// if (myAge >= votingAge) 
+// {
+//   Console.WriteLine("Old enough to vote!");
+// } 
+// else 
+// {
+//   Console.WriteLine("Not old enough to vote.");
+// };
+
+// int age = 45;
+
+// switch(age){
+
+//     case ((age > 30)):
+//         Console.WriteLine("HIiiiiii");
+//     break;
+
+//     default:
+//     // code block
+//     break;
+// }
+
+
+// int i = 0;
+// while (i < 5) 
+// {
+//   Console.WriteLine(i);
+//   i++;
+// }
+
+
+// for(int i = 1 ; i <= 5 ; i++){
+    
+//     for (int j= 1 ; j <= i ; j++){
+
+//         Console.Write(" * ");
+//     }
+
+//       Console.WriteLine();
+// }
+
+// int i = 0;
+// do 
+// {
+//   Console.WriteLine(i);
+//   i++;
+// }
+// while (i < 5);
+
+
+// string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+
+// foreach (string car in cars){
+
+//     Console.WriteLine(car);
+// }
+
+
+// for (int i = 0; i < 10; i++) 
+// {
+//   if (i == 4) 
+//   {
+//     break;
+//   }
+//   Console.WriteLine(i);
+// }
+
+
+// for (int i = 0; i < 10; i++) 
+// {
+//   if (i == 4) 
+//   {
+//     continue;
+//   }
+//   Console.WriteLine(i);
+// }
+
+// Sort an int
+// int[] myNumbers = {5, 1, 8, 9};
+// Array.Sort(myNumbers);
+// foreach (int i in myNumbers)
+// {
+//   Console.WriteLine(i);
+// }
+
+
+
+///Defile Main Method
+///
+
+
+// using System;
+
+// namespace HelloWorldApplication {
+//    class HelloWorld {
+//       static void Main(string[] args) {
+//          /* my first program in C# */
+//          Console.WriteLine("Hello World");
+//       }
+//    }
+// }
+
+
+// class Car 
+// {
+//   string color = "red";
+
+//   static void Main(string[] args)
+//   {
+//     Car myObj = new Car();
+//     Console.WriteLine(myObj.color);
+//   }
+// }
+
+
+// class Car
+// {
+//   string color = "red";
+//   static void Main(string[] args)
+//   {
+//     Car myObj1 = new Car();
+//     Car myObj2 = new Car();
+//     Console.WriteLine(myObj1.color);
+//     Console.WriteLine(myObj2.color);
+//   }
+// }
+
+
+
+class Car {
+
+    private int carNo;
+    private string carModel;
+
+    
+}
