754bead796c280e2631e3ae7e4daf9e049c22ff0
Mihiraj <mihiraj@Mihirajs-MBP.localdomain>
2024-09-10 08:51:13 +1000

    Changes added 10-09-2024

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿// Console.WriteLine("Enter Your Name Here ");
+﻿using System;
+using myApp;
+
+// Console.WriteLine("Enter Your Name Here ");
 
 // string firtsName = Console.ReadLine();
 
@@ -60,7 +63,7 @@
 
 
 // for(int i = 1 ; i <= 5 ; i++){
-    
+
 //     for (int j= 1 ; j <= i ; j++){
 
 //         Console.Write(" * ");
@@ -158,10 +161,107 @@
 
 
 
-class Car {
+// class Car {
 
-    private int carNo;
-    private string carModel;
+//     private int carNo;
+//     private string carModel;
 
-    
+//      public int CarNo   // property
+//         {
+//             get { return carNo; }
+//             set { carNo = value; }
+//         }
+
+//     public string CarModel   // property
+//         {
+//             get { return carModel; }
+//             set { carModel = value; }
+//         }
+
+
+// }
+
+
+// class Program
+// {
+//   static void Main(string[] args)
+//   {
+//     Car myObj = new Car();
+//     myObj.CarModel = "Volvo";
+
+
+//     Console.WriteLine(myObj.CarModel);
+//   }
+//}
+
+//Inheritnace
+
+
+// using System;
+
+// namespace myApp {
+//     class Vehicle {
+
+//     public string vehicleTCode;
+
+
+//     public string VehicleCode{
+//         get;
+//         set;
+//     }
+// }
+
+
+// class  MotorVehicle : Vehicle{
+//     public string  color;
+
+
+//     public string Color{
+//         get;
+//         set;
+//     }
+// }
+
+
+//  static void Main (string[] args){
+
+//     MotorVehicle motorVehicle = new MotorVehicle();
+
+//     motorVehicle.vehicleTCode = "CR";
+//     motorVehicle.color = "Red";
+
+//     Console.WriteLine(motorVehicle.Color);
+// }
+
+// }
+
+
+namespace myApp {
+
+
+    abstract class Animal{
+
+
+       public abstract void animalName();
+
+        public void animalColor(string name){
+            Console.WriteLine(name);
+        }
+    }
+
+
+    class Pig : Animal {
+        public override void animalName(){
+            Console.WriteLine("Piff");
+        }
+    }
+
+    class Program{
+    static void Main(string[] args)
+    {
+      Pig myPig = new Pig();  // Create a Pig object
+      myPig.animalName();
+      myPig.animalColor("Red");
+    }
+  }
 }
